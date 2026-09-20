a9f4b06d4735486bba4ca6c84f056df21c9f13c2
Iván Emmanuel Gómez <ivanemmanuelgomez2017@gmail.com>
2026-09-20 17:58:02 -0300

    feat(users): se agregó el registro de usuarios

--- backend/src/models/Profile.js
+++ backend/src/models/Profile.js
@@ -3,7 +3,7 @@ import sequelize from '../config/database.js'
 
 const Profile = sequelize.define('Profile', {
   id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
-  first_name: { type: DataTypes.STRING(80), allowNull: false },
+  name: { type: DataTypes.STRING(80), allowNull: false },
   last_name: { type: DataTypes.STRING(80), allowNull: false },
   nickname: { type: DataTypes.STRING(80) },
   avatar_url: { type: DataTypes.STRING(500) },
